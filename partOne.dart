c82cc16926d973c8dcdd006692371b0c9c0aa450
Cesar Sandoval <cesar.sandoval.soto@icloud.com>
2023-11-15 22:42:34 -0300

    dart0

---
+++ partOne.dart
@@ -0,0 +1,20 @@
+void main() {
+  final int numero = 10;
+  final String pokemon = "parras";
+  final double decimal = 1.1;
+  final ataques = ["garra", "susto", "rapido"];
+
+  dynamic errorMessage = "mensaje de error";
+  errorMessage = 404;
+  errorMessage = true;
+  errorMessage = [9,8,7,6];
+
+  print("""
+  $numero
+  $pokemon
+  $decimal
+  $ataques
+  $errorMessage
+  
+  """);
+}
