d71f005905b936c7c580991af45d36bc4fc7f2c0
Cesar Sandoval <cesar.sandoval.soto@icloud.com>
2023-11-19 00:24:54 -0300

    caja de arena

---
+++ miCajaDeArena.dart
@@ -0,0 +1,10 @@
+void main (){
+  final Map <String, dynamic> gatos = {
+    "nombre" : ["Simona", "Orion", "Rudy", "Mia"],
+    "edad": [1, 2, 0, 6],
+    "peso" :["4,2 kg", "4,8 kg", "3,0 kg", "4,0 kg"]
+  };
+
+  print ("La gatita ${gatos["nombre"][0]} tiene ${gatos["edad"][0]} años" );
+  print ("y pesa ${gatos["peso"][0]}");
+}
