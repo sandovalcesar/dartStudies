834ab90d1a1a4728a10268f561c4585476cae291
Cesar Sandoval <cesar.sandoval.soto@icloud.com>
2023-11-21 23:36:28 -0300

    Funciones con named parameted

--- partFour.dart
+++ partFour.dart
@@ -2,6 +2,8 @@ void main (){
 
   print (greetEvyone());
   print ("Suma : ${addTwoNumbers(10, 20)}");
+
+  print(greetingPerson(name: "Cesar", message: "hola"));
 }
 
 String greetEvyone () => "Hello everyone";
@@ -10,4 +12,8 @@ int addTwoNumbers(int a, int b) => a + b;
 
 int addOptinalTwoNumbers(int a, [int b = 0]) { //impide que el valor B no sea insegura
   return a + b;
+}
+
+String greetingPerson ({ String? name, String? message}){
+return "$name, $message";
 }
